f5b1a973fe5b800c0df794fa127e10330440f1f8
Summer Ficarrotta <sficarrotta@rallydev.com>
2012-12-12 15:33:03 -0500

    snapshot store addede

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
-Ext.define('CustomApp', {
+Ext.define('DefectTrendRemixedApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
 
     launch: function() {
-        //Write app code here
-    }
-});
+      Ext.create('Rally.data.lookback.SnapshotStore', {
+          autoLoad: true,
+          listeners: {
+            load: function(store, data, success) {
+                console.log('Loaded Data', data);
+            }
+        },
+        fetch: ['Name', 'State', 'Owner'],
+        filters: [
+            {
+                property: '_TypeHierarchy',
+                operator: 'in',
+                value: ['Defect']
+            }
+        ]
+    });
+
+}});
+    
+

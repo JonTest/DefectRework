f51ceaa1dc7d4994b2e034e85c406606117c4e2f
David P Thomas <davidpthomas@gmail.com>
2012-12-13 22:33:39 -0800

    chart colors

--- Chart.js
+++ Chart.js
@@ -2,39 +2,31 @@ Ext.define('DefectTrendRemixedApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
 
+
+
     _loadChart: function() {
-      this.add({
-        xtype: 'rallychart',
-        height: 400,
-        series: [{
-            name: 'Reopened',
-            data: [5, 3, 4, 7, 2],
-            stack: 'arrival'
-        }, {
-            name: 'New',
-            data: [3, 4, 4, 2, 5],
-            stack: 'arrival'
-        }, {
-            name: 'Rejected',
-            data: [2, 5, 6, 2, 1],
-            stack: 'kill'
-        }, {
-            name: 'Closed',
-            data: [3, 6, 4, 4, 3],
-            stack: 'kill'
-        }],
-        chartConfig: {
+/**
+ * Grid theme for Highcharts JS
+ * @author Torstein Hønsi
+ */
+
+       chartConfig = {
             chart: {
                 type: 'column'
             },
-    
+            colors: [
+              '#FF0000', // reopened: red
+              '#C3F500', // new: green
+              '#DDDDDD', // rejected: grey
+              '#333086', // closed: blue
+              '#3D96AE', 
+              '#DB843D', 
+              '#92A8CD', 
+              '#A47D7C', 
+              '#B5CA92'
+            ],
             title: {
                 text: 'Defect Arrival / Kill'
-            },
-            credits: {
-              enabled: true,
-              href: 'http://www.rallydev.com',
-              text: 'Rally Software'
             },
             xAxis: {
                 categories: ['01/01/2012', '01/02/2012', '01/03/2012', '01/04/2012', '01/05/2012'],
@@ -58,6 +50,24 @@ Ext.define('DefectTrendRemixedApp', {
                 }
             },
     
+          series: [{
+              name: 'Reopened',
+              data: [5, 3, 4, 7, 2],
+              stack: 'arrival'
+          }, {
+              name: 'New',
+              data: [3, 4, 4, 2, 5],
+              stack: 'arrival'
+          }, {
+              name: 'Rejected',
+              data: [2, 5, 6, 2, 1],
+              stack: 'kill'
+          }, {
+              name: 'Closed',
+              data: [3, 6, 4, 4, 3],
+              stack: 'kill'
+          }],
+
             plotOptions: {
                 area: {
                     marker: {
@@ -75,8 +85,16 @@ Ext.define('DefectTrendRemixedApp', {
                     stacking: 'normal'
                 }
             }
-        }
-      });
+        };
+
+      chart = {
+        xtype: 'rallychart',
+        height: 400,
+        chartConfig: chartConfig
+      };
+
+      this.add(chart);
+
     },
     launch: function() {
       this._loadChart();
